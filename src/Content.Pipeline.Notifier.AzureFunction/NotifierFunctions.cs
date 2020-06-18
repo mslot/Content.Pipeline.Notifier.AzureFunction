2af75523d1472d82003d6f5d91213e03d9a3f879
Martin Slot <slot@sigsys.dk>
2020-06-18 19:43:47 +0200

    Initial commit

---
+++ src/Content.Pipeline.Notifier.AzureFunction/NotifierFunctions.cs
@@ -0,0 +1,26 @@
+using System.Threading.Tasks;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Extensions.Logging;
+using System;
+
+namespace Content.Pipeline.Notifier.AzureFunction
+{
+    public class NotifierFunctions
+    {
+        [FunctionName("NotiferFunction")]
+        public async Task Run(
+        [ServiceBusTrigger("notifier-topic","NotifierSubscription", Connection = "Servicebus:ServicebusConnectionString")]
+        string message,
+        Int32 deliveryCount,
+        DateTime enqueuedTimeUtc,
+        string messageId,
+        ILogger log)
+        {
+            log.LogInformation($"C# ServiceBus queue trigger function processed message: {message}");
+            log.LogInformation($"EnqueuedTimeUtc={enqueuedTimeUtc}");
+            log.LogInformation($"DeliveryCount={deliveryCount}");
+            log.LogInformation($"MessageId={messageId}");
+
+        }
+    }
+}
---
+++ src/Content.Pipeline.Notifier.AzureFunction/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Azure.Functions.Extensions.DependencyInjection;
+
+[assembly: FunctionsStartup(typeof(Content.Pipeline.Notifier.AzureFunction.Startup))]
+namespace Content.Pipeline.Notifier.AzureFunction
+{
+    public class Startup : FunctionsStartup
+    {
+        public override void Configure(IFunctionsHostBuilder builder)
+        {
+        }
+    }
+}
