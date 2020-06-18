eeb9ab2bd1295206293f5cd10d6a7cf5cfdff470
Martin Slot <slot@sigsys.dk>
2020-06-18 23:01:15 +0200

    Added eventgrid return

--- src/Content.Pipeline.Notifier.AzureFunction/NotifierFunctions.cs
+++ src/Content.Pipeline.Notifier.AzureFunction/NotifierFunctions.cs
@@ -2,13 +2,17 @@ using System.Threading.Tasks;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Extensions.Logging;
 using System;
+using Microsoft.Azure.WebJobs.Extensions.EventGrid;
+using Microsoft.Azure.EventGrid.Models;
+using System.Collections.Generic;
 
 namespace Content.Pipeline.Notifier.AzureFunction
 {
     public class NotifierFunctions
     {
         [FunctionName("NotiferFunction")]
-        public void Run(
+        [return: EventGrid(TopicEndpointUri = "EventGrid:TopicUri", TopicKeySetting = "EventGrid:TopicKey")]
+        public Task Run(
         [ServiceBusTrigger("notifier-topic","NotifierSubscription", Connection = "Servicebus:ServicebusConnectionString")]
         string message,
         Int32 deliveryCount,
@@ -21,6 +25,11 @@ namespace Content.Pipeline.Notifier.AzureFunction
             log.LogInformation($"DeliveryCount={deliveryCount}");
             log.LogInformation($"MessageId={messageId}");
 
+            var random = new Random();
+            List<string> eventTypes = new List<string> { "UpSertMetadata", "UpsertTheme", "UpsertVideoUrl", "DeleteMetadata" };
+            string eventType = eventTypes[random.Next(eventTypes.Count)];
+
+            return Task.FromResult(new EventGridEvent(messageId, eventType, message, eventType, DateTime.UtcNow, "1.0"));
         }
     }
 }
